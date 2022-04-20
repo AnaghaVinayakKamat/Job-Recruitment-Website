39bb3158d78c01cf1b47a3a43e684c5879ea7206
Anagha Kamat <84722023+AnaghaVinayakKamat@users.noreply.github.com>
2022-04-20 11:49:52 +0530

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -15,17 +15,5 @@ admin.site.register(Com_det, AdminCompany)
 admin.site.register(Can_det, Can_detAdmin)
 admin.site.register(Resume)
 admin.site.register(Job_det)
-
-
-# class AnswerAdmin(admin.StackedInline):
-#     model = Answer
-
-# class QuestionAdmin(admin.ModelAdmin):
-#     inlines=[AnswerAdmin]
-
-
-# admin.site.register(Question, QuestionAdmin)
-# admin.site.register(Answer)
-# admin.site.register(Quiz)
 admin.site.register(Apply_job)
 admin.site.register(Decision)
